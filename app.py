a7ebe8aeb856091e8f3acaa7090475212964c02e
yming <1136476066@qq.com>
2023-02-19 15:53:08 +0800

    add_name

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import json
 
+import pandas as pd
 from flask import Flask, request
 
 import Ashare
@@ -7,17 +8,27 @@ import MyTT
 
 app = Flask(__name__)
 
+code_name = pd.read_csv('code_name.csv')
+
 
 @app.route('/request', methods=["GET"])
 def request_info():
     code = request.args.get("code")
     df = Ashare.get_price(code=code, count=365)
     data = {"now_price": str(df.close[-1]),
+            "name": request_code_name(code),
             "MA_180": str(MyTT.MA(df.close.values, 180)[-1]),
             "MA_100": str(MyTT.MA(df.close.values, 100)[-1]),
             "MA_30": str(MyTT.MA(df.close.values, 30)[-1]),
             "MA_7": str(MyTT.MA(df.close.values, 7)[-1])}
     return json.dumps(data)
+
+
+def request_code_name(code):
+    try:
+        return code_name[code_name['code'] == int(code[-6:])]['name'][0]
+    except:
+        return "股票" + code
 
 
 if __name__ == '__main__':
